f934e9f9e4993932d2a0d6a8d4fdef76dffa0ee7
KELLY FAM <kellyfam@microsoft.com>
2024-05-06 14:40:04 -0700

    Add sample task for 17.10

---
+++ New_Extensibility_Model/Samples/FilePickerSample/FilePickerSampleExtension.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace FilePickerSample;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+/// <summary>
+/// Extension entry point for the FilePickerSample.
+/// </summary>
+[VisualStudioContribution]
+public class FilePickerSampleExtension : Extension
+{
+    /// <inheritdoc/>
+    public override ExtensionConfiguration ExtensionConfiguration => new()
+    {
+        Metadata = new(
+                id: "FilePickerCommandSample.e9bc432a-5be1-43e8-b7d8-bc7b7024326d",
+                version: this.ExtensionAssemblyVersion,
+                publisherName: "Microsoft",
+                displayName: "File Picker Sample Extension",
+                description: "Sample extension demonstrating the file picker prompt capabilities"),
+    };
+
+    /// <inheritdoc/>
+    protected override void InitializeServices(IServiceCollection serviceCollection)
+    {
+        base.InitializeServices(serviceCollection);
+    }
+}
---
+++ New_Extensibility_Model/Samples/FilePickerSample/OpenFileCommand.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace FilePickerSample;
+
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell.FileDialog;
+
+[VisualStudioContribution]
+public class OpenFileCommand(VisualStudioExtensibility extensibility) : Command(extensibility)
+{
+    public override CommandConfiguration CommandConfiguration => new("%FilePickerSample.OpenFileCommand.DisplayName%")
+    {
+        TooltipText = "%FilePickerSample.OpenFileCommand.TooltipText%",
+        Placements = [CommandPlacement.KnownPlacements.ToolsMenu],
+        Icon = new(ImageMoniker.KnownValues.OpenFileDialog, IconSettings.IconAndText),
+    };
+
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken ct)
+    {
+        FileDialogOptions options = new()
+        {
+            InitialFileName = "test.cs",
+            Filters = new DialogFilters([
+                new("Log Files", "*.txt", "*.log"),
+                new("CSharp Files", "*.cs"),
+            ])
+            {
+                DefaultFilterIndex = 1,
+            },
+        };
+        string? filePath = await this.Extensibility.Shell().ShowOpenFileDialogAsync(options, ct);
+        Debug.WriteLine($"Selected file path: {filePath ?? "No selection"}");
+    }
+}
