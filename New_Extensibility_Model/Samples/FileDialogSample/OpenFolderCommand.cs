99862a5fc2dc4002c81ca8cfe9d9f01cfb6c589d
KELLY FAM <kellyfam@microsoft.com>
2024-05-06 14:40:04 -0700

    Add sample task for 17.10

---
+++ New_Extensibility_Model/Samples/FileDialogSample/OpenFolderCommand.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace FilePickerSample;
+
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell.FileDialog;
+
+[VisualStudioContribution]
+public class OpenFolderCommand(VisualStudioExtensibility extensibility) : Command(extensibility)
+{
+    public override CommandConfiguration CommandConfiguration => new("%FilePickerSample.OpenFolderCommand.DisplayName%")
+    {
+        TooltipText = "%FilePickerSample.OpenFolderCommand.TooltipText%",
+        Placements = [CommandPlacement.KnownPlacements.ToolsMenu],
+        Icon = new(ImageMoniker.KnownValues.OpenFileDialog, IconSettings.IconAndText),
+    };
+
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken ct)
+    {
+        FolderDialogOptions options = new();
+        string? folderPath = await this.Extensibility.Shell().ShowOpenFolderDialogAsync(options, ct);
+        Debug.WriteLine($"Selected folder path: {folderPath ?? "No selection"}");
+    }
+}
