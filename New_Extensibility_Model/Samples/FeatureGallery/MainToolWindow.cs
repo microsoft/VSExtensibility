7e0956bde7d94fcac459c496279c5026db8f1bf9
Matteo Prosperi <maprospe@microsoft.com>
2024-07-09 11:19:43 -0700

    Adding a test for settings

--- New_Extensibility_Model/Samples/FeatureGallery/MainToolWindow.cs
+++ New_Extensibility_Model/Samples/FeatureGallery/MainToolWindow.cs
@@ -38,6 +38,7 @@ public class MainToolWindow : ToolWindow
             new ModalDialogTest(this.Extensibility),
             new EditorMarginTest(this.Extensibility),
             new LanguageServerTest(this.Extensibility),
+            new SettingsTest(this.Extensibility),
         };
     }
 
---
+++ New_Extensibility_Model/Samples/FeatureGallery/Settings/SettingsTest.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if INPROC
+namespace InProcFeatureGallery;
+#else
+namespace FeatureGallery;
+#endif
+
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Settings;
+using Microsoft.VisualStudio.Extensibility.Shell;
+
+[DataContract]
+internal class SettingsTest : TestData
+{
+    public SettingsTest(VisualStudioExtensibility extensibility)
+        : base(extensibility)
+    {
+    }
+
+    [DataMember]
+    public override string ButtonText => "Update a setting value";
+
+    [DataMember]
+    public override string Description => "This command updates a setting value and shows a prompt when the value update is complete.";
+
+    [VisualStudioContribution]
+#pragma warning disable VSEXTPREVIEW_SETTINGS // Type is for evaluation purposes only and is subject to change or removal in future updates.
+#pragma warning disable CEE0027 // String not localized
+#if INPROC
+    private static SettingCategory Category { get; } = new("inProcFeatureGallery", "In-Proc Feature Gallery");
+#else
+    private static SettingCategory Category { get; } = new("featureGallery", "Feature Gallery");
+#endif
+
+    [VisualStudioContribution]
+    private static Setting.Boolean BooleanSetting { get; } = new("booleanSetting", "Boolean Setting", Category, defaultValue: false);
+#pragma warning restore CEE0027 // String not localized
+
+    protected override async Task RunAsync(IClientContext clientContext, CancellationToken cancellationToken)
+    {
+        await this.Extensibility.Settings().WriteAsync(
+            batch => batch.WriteSetting(BooleanSetting, false),
+            "Setting booleanSetting to false",
+            cancellationToken);
+
+        await WaitForSettingValueAsync(expectedValue: false);
+
+        await this.Extensibility.Settings().WriteAsync(
+            batch => batch.WriteSetting(BooleanSetting, true),
+            "Setting booleanSetting to true",
+            cancellationToken);
+
+        await WaitForSettingValueAsync(expectedValue: true);
+
+        async Task WaitForSettingValueAsync(bool expectedValue)
+        {
+            TaskCompletionSource<bool> settingValueIsFalse = new();
+            using var settingValueObserver = await this.Extensibility.Settings().SubscribeAsync(BooleanSetting, cancellationToken, value =>
+            {
+                if (value.Succeeded && value.Value == expectedValue)
+                {
+                    settingValueIsFalse.SetResult(true);
+                }
+            });
+
+            await settingValueIsFalse.Task;
+        }
+
+        await this.Extensibility.Shell().ShowPromptAsync("All done.", PromptOptions.OK, cancellationToken);
+    }
+#pragma warning restore VSEXTPREVIEW_SETTINGS // Type is for evaluation purposes only and is subject to change or removal in future updates.
+}
