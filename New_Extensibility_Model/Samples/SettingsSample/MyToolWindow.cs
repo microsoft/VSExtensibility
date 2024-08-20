493678062fb64229eebd75d60f771269ed4a8cbb
Charles Willis <chawill@microsoft.com>
2024-08-20 18:55:14 +0100

    Add sample for Settings API

---
+++ New_Extensibility_Model/Samples/SettingsSample/MyToolWindow.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace SettingsSample;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.ToolWindows;
+using Microsoft.VisualStudio.RpcContracts.RemoteUI;
+
+/// <summary>
+/// A sample tool window.
+/// </summary>
+[VisualStudioContribution]
+public class MyToolWindow : ToolWindow
+{
+    private object? dataContext;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyToolWindow" /> class.
+    /// </summary>
+    public MyToolWindow()
+    {
+        this.Title = "Settings Sample Tool Window";
+    }
+
+    /// <inheritdoc />
+    public override ToolWindowConfiguration ToolWindowConfiguration => new()
+    {
+        Placement = ToolWindowPlacement.DocumentWell,
+        AllowAutoCreation = false,
+    };
+
+    /// <inheritdoc />
+    public override Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        this.dataContext = new MyToolWindowData(this.Extensibility);
+
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public override Task<IRemoteUserControl> GetContentAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult<IRemoteUserControl>(new MyToolWindowControl(this.dataContext));
+    }
+}
---
+++ New_Extensibility_Model/Samples/SettingsSample/SettingDefinitions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace SettingsSample;
+
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Settings;
+
+#pragma warning disable VSEXTPREVIEW_SETTINGS // The settings API is currently in preview and marked as experimental
+
+internal static class SettingDefinitions
+{
+    [VisualStudioContribution]
+    internal static SettingCategory SettingsSampleCategory { get; } = new("settingsSample", "%SettingsSample.Settings.Category.DisplayName%")
+    {
+        Description = "%SettingsSample.Settings.Category.Description%",
+    };
+
+    [VisualStudioContribution]
+    internal static Setting.Boolean AutoUpdateSetting { get; } = new("autoUpdate", "%SettingsSample.Settings.AutoUpdate.DisplayName%", SettingsSampleCategory, defaultValue: true)
+    {
+        Description = "%SettingsSample.Settings.AutoUpdate.Description%",
+    };
+
+    [VisualStudioContribution]
+    internal static Setting.Integer TextLengthSetting { get; } = new("textLength", "%SettingsSample.Settings.TextLength.DisplayName%", SettingsSampleCategory, defaultValue: 10)
+    {
+        Description = "%SettingsSample.Settings.TextLength.Description%",
+        Minimum = 1,
+        Maximum = MyToolWindowData.LoremIpsumText.Length,
+    };
+
+    [VisualStudioContribution]
+    internal static Setting.Enum QuoteStyleSetting { get; } = new(
+        "quoteStyle",
+        "%SettingsSample.Settings.QuoteStyle.DisplayName%",
+        SettingsSampleCategory,
+        [new EnumSettingEntry("none", "%SettingsSample.Settings.QuoteStyle.None%"), new EnumSettingEntry("single", "%SettingsSample.Settings.QuoteStyle.Single%"), new EnumSettingEntry("double", "%SettingsSample.Settings.QuoteStyle.Double%")],
+        defaultValue: "double")
+    {
+        Description = "%SettingsSample.Settings.QuoteStyle.Description%",
+    };
+}
