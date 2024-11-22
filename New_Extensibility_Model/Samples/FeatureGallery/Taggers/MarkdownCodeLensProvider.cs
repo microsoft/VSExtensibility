0c8590d6da4bb177e0f3f25d70a88654cdd28bc0
Matteo Prosperi <maprospe@microsoft.com>
2024-11-22 11:53:38 -0800

    Taggers and CodeLens manual tests

---
+++ New_Extensibility_Model/Samples/FeatureGallery/Taggers/MarkdownCodeLensProvider.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if INPROC
+namespace InProcFeatureGallery;
+#else
+namespace FeatureGallery;
+#endif
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Editor;
+
+#pragma warning disable VSEXTPREVIEW_CODELENS // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+/// <summary>
+/// A sample CodeLens provider that shows the number of words in a method or type and provides custom UI when clicked.
+/// </summary>
+[VisualStudioContribution]
+internal class MarkdownCodeLensProvider : ExtensionPart, ICodeLensProvider
+{
+    public TextViewExtensionConfiguration TextViewExtensionConfiguration => new()
+    {
+        AppliesTo = [DocumentFilter.FromDocumentType(TaggerTest.MyMarkdownDocumentType)],
+    };
+
+#pragma warning disable CEE0027 // String not localized
+    public CodeLensProviderConfiguration CodeLensProviderConfiguration =>
+        new("Remote UI CodeLens Sample Provider")
+        {
+            Priority = 600,
+        };
+#pragma warning restore CEE0027 // String not localized
+
+    public Task<CodeLens?> TryCreateCodeLensAsync(CodeElement codeElement, CodeElementContext codeElementContext, CancellationToken token)
+    {
+        if (codeElement.Kind == MarkdownCodeLensTagger.SectionCodeElementKind)
+        {
+            return Task.FromResult<CodeLens?>(new MarkdownCodeLens(codeElement));
+        }
+
+        return Task.FromResult<CodeLens?>(null);
+    }
+}
---
+++ New_Extensibility_Model/Samples/FeatureGallery/Taggers/TaggerTest.cs
@@ -0,0 +1,83 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if INPROC
+namespace InProcFeatureGallery;
+#else
+namespace FeatureGallery;
+#endif
+
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Editor;
+using Microsoft.VisualStudio.Extensibility.Shell;
+using Microsoft.VisualStudio.RpcContracts.Documents;
+
+[DataContract]
+internal class TaggerTest : TestData
+{
+#if INPROC
+    public const string FileExtension = "myinprocmd";
+#else
+    public const string FileExtension = "mymd";
+#endif
+
+    public TaggerTest(VisualStudioExtensibility extensibility)
+        : base(extensibility)
+    {
+    }
+
+    [VisualStudioContribution]
+    public static DocumentTypeConfiguration MyMarkdownDocumentType => new(FileExtension)
+    {
+        FileExtensions = ["." + FileExtension],
+        BaseDocumentType = DocumentType.KnownValues.Text,
+    };
+
+    [DataMember]
+    public override string ButtonText => "Taggers and CodeLens";
+
+    [DataMember]
+    public override string Description => "This command opens a markdown file. Every title in the file should be highlighted and have a CodeLens showing its ID on top of it. A prompt will ask to close the file.";
+
+    protected override async Task RunAsync(IClientContext clientContext, CancellationToken cancellationToken)
+    {
+        var filePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + "." + FileExtension);
+        bool keepFile = false;
+
+        using var file = new StreamWriter(File.Create(filePath));
+        try
+        {
+            await file.WriteAsync("""
+                # Title 1
+
+                ## Title 2
+                Some text.
+                Some more text.
+
+                # Title 3
+                Even more text.
+                """);
+            file.Close();
+
+            var textDocumentSnapshot = await this.Extensibility.Documents().OpenTextDocumentAsync(new(filePath), cancellationToken);
+
+            if (!await this.Extensibility.Shell().ShowPromptAsync("Press OK to close and delete the file.", PromptOptions.OKCancel, cancellationToken))
+            {
+                keepFile = true;
+            }
+        }
+        finally
+        {
+            if (!keepFile)
+            {
+                await this.Extensibility.Documents().CloseDocumentAsync(new(filePath), SaveDocumentOption.NoSave, cancellationToken);
+                File.Delete(filePath);
+            }
+        }
+    }
+}
