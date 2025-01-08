0a36c7695c4ed5f41d14d79be69bebe1592babca
Murphy McQuet <murphymcquet@microsoft.com>
2025-01-08 10:24:33 -0800

    Adding sample to test .NET target

---
+++ New_Extensibility_Model/Samples/RuntimeTargetSample/Command1.cs
@@ -0,0 +1,49 @@
+﻿namespace RuntimeTargetSample;
+
+using System.Diagnostics;
+using Microsoft;
+using Microsoft.VisualStudio.Extensibility;
+using Microsoft.VisualStudio.Extensibility.Commands;
+using Microsoft.VisualStudio.Extensibility.Shell;
+
+/// <summary>
+/// Command1 handler.
+/// </summary>
+[VisualStudioContribution]
+internal class Command1 : Command
+{
+    private readonly TraceSource logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Command1"/> class.
+    /// </summary>
+    /// <param name="traceSource">Trace source instance to utilize.</param>
+    public Command1(TraceSource traceSource)
+    {
+        // This optional TraceSource can be used for logging in the command. You can use dependency injection to access
+        // other services here as well.
+        this.logger = Requires.NotNull(traceSource, nameof(traceSource));
+    }
+
+    /// <inheritdoc />
+    public override CommandConfiguration CommandConfiguration => new("%RuntimeTargetSample.Command1.DisplayName%")
+    {
+        // Use this object initializer to set optional parameters for the command. The required parameter,
+        // displayName, is set above. DisplayName is localized and references an entry in .vsextension\string-resources.json.
+        Icon = new(ImageMoniker.KnownValues.Extension, IconSettings.IconAndText),
+        Placements = [CommandPlacement.KnownPlacements.ExtensionsMenu],
+    };
+
+    /// <inheritdoc />
+    public override Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        // Use InitializeAsync for any one-time setup or initialization.
+        return base.InitializeAsync(cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public override async Task ExecuteCommandAsync(IClientContext context, CancellationToken cancellationToken)
+    {
+        await this.Extensibility.Shell().ShowPromptAsync($"Executed on a host with DOTNET_ROOT: {Environment.GetEnvironmentVariable("DOTNET_ROOT")}!", PromptOptions.OK, cancellationToken);
+    }
+}
---
+++ New_Extensibility_Model/Samples/RuntimeTargetSample/ExtensionEntrypoint.cs
@@ -0,0 +1,35 @@
+﻿namespace RuntimeTargetSample;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+/// <summary>
+/// Extension entrypoint for the VisualStudio.Extensibility extension.
+/// </summary>
+[VisualStudioContribution]
+internal class ExtensionEntrypoint : Extension
+{
+    /// <inheritdoc/>
+    public override ExtensionConfiguration ExtensionConfiguration => new()
+    {
+        Metadata = new(
+                id: "RuntimeTargetSample.e70fad4f-7d9b-4f06-ae6c-9583811dd956",
+                version: this.ExtensionAssemblyVersion,
+                publisherName: "Microsoft",
+                displayName: "Runtime Target Sample",
+                description: "Test different .NET targets")
+        {
+            // DotnetTargetVersions = null,                 // <-- Comment out all options or leave this option to use default value
+            // DotnetTargetVersions = [DotnetTarget.Net8],  // <-- Selects a valid value
+            // DotnetTargetVersions = ["net6.0"],           // <-- Compilation warning will be raised because the extension is built targeting .NET 8
+        },
+    };
+
+    /// <inheritdoc />
+    protected override void InitializeServices(IServiceCollection serviceCollection)
+    {
+        base.InitializeServices(serviceCollection);
+
+        // You can configure dependency injection here by adding services to the serviceCollection.
+    }
+}
