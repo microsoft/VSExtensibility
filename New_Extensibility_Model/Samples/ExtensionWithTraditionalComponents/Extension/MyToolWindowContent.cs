123eb3a2a9c49dfbc354f75318593c9cc2515b3f
Matteo Prosperi <maprospe@microsoft.com>
2025-03-31 13:51:41 -0700

    Add a sample for an out-of-proc extension using a custom WPF user control

---
+++ New_Extensibility_Model/Samples/ExtensionWithTraditionalComponents/Extension/MyToolWindowContent.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace ExtensionWithTraditionalComponents;
+
+using Microsoft.VisualStudio.Extensibility.UI;
+
+/// <summary>
+/// A remote user control to use as tool window UI content.
+/// </summary>
+internal class MyToolWindowContent : RemoteUserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MyToolWindowContent" /> class.
+    /// </summary>
+    public MyToolWindowContent()
+        : base(dataContext: new MyToolWindowData())
+    {
+    }
+}
