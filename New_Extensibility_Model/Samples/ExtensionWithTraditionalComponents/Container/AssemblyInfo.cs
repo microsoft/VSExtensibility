c1a97652cb71b4f9f8c0bb23ab3722315fc5b296
Matteo Prosperi <maprospe@microsoft.com>
2025-04-22 17:15:36 -0700

    Docs

--- New_Extensibility_Model/Samples/ExtensionWithTraditionalComponents/Container/AssemblyInfo.cs
+++ New_Extensibility_Model/Samples/ExtensionWithTraditionalComponents/Container/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
